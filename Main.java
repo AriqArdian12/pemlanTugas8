e1765165aad7ffac0cfaf36e883354a7d33d4f50
CrypHunt <artalarik2017@gmail.com>
2025-05-16 20:53:30 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Set look and feel untuk tampilan yang lebih baik
+        // Agar tampilan terlihat lebih baik
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
         }
         
-        // Jalankan aplikasi
+        // Menjalankan file
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
